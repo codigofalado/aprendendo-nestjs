29f426caafba39c520476a7933224b00f80d94e0
Fernando Santos <fern@ndo.io>
2020-05-31 01:07:37 -0300

    Adicionando Validação Server-Side

--- src/crud/create-food.dto.ts
+++ src/crud/create-food.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty } from "class-validator";
 export class CreateFoodDto {
+  @IsNotEmpty()
   name: string;
+  @IsNotEmpty()
   ingredients: string;
+  @IsNotEmpty()
   emoji: string;
+  @IsNotEmpty()
   price: number;
 }
